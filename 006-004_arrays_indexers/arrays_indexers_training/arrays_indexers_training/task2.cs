3a25f8eac4c92ab1f26ef35e4258b23dbe69c402
RicoygLex <ricoyserglex@gmail.com>
2023-09-02 18:44:17 +0300

    add .gitignore and start of arrays_indexers

---
+++ 006-004_arrays_indexers/arrays_indexers_training/arrays_indexers_training/task2.cs
@@ -0,0 +1,27 @@
+﻿static void Main()
+{
+    int n;
+
+    Console.WriteLine("Введите длину массива");
+    n = Console.Read();
+
+    if (n < 0 && n == 0) 
+        Console.WriteLine("Длина массива неверна, попробуй другую цифру");
+
+    int[] jagged = new int[n];
+
+    // Во внешнем цикле выполняется проход по всем вложенным массивам.
+    for (int i = 0; i < jagged.Length; ++i)
+    {
+        // Во внутреннем цикле выполняется обращение к каждому элементу вложенного массива.
+        for (int j = 0; j < jagged[i].Length; ++j)
+        {
+            Console.Write("{0} ", jagged[i][j]);
+        }
+        Console.Write("\n");
+    }
+
+
+    // Delay.
+    Console.ReadKey();
+}
